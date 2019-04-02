7b0774c000186255bf7807d1168a41812c98c33e
Luan Bitar <bitar.luan21@gmail.com>
2019-04-02 18:59:51 -0400

    removing index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import { compose, setDisplayName, withState } from 'recompose'
-
-export default compose(
-  setDisplayName('/src/enhancers/recompose-snippets/index.js'),
-  withState('$1', 'set${1:/capitalize}', $2),
-  $0
-)
